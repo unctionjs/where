5bcf07fa3a7bd44f36fef8a9ff21f035b6fb2f6b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 14:01:40 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-import reduceWithValueKey from "@unction/reducewithvaluekey"
-import dig from "@unction/dig"
-import arrayify from "@unction/arrayify"
-
-import type {KeyedEnumerableType} from "types"
-import type {PredicateFunctionType} from "types"
-import type {UnaryFunctionType} from "types"
-import type {KeyType} from "types"
-
-export default function where (matcher: KeyedEnumerableType<PredicateFunctionType>): UnaryFunctionType {
-  return function whereMatcher (keyedEnumerable: KeyedEnumerableType): boolean {
-    return reduceWithValueKey(
-      (latest: boolean): UnaryFunctionType =>
-        (value: PredicateFunctionType): UnaryFunctionType =>
-          (key: KeyType): boolean => latest && value(dig(arrayify(key))(keyedEnumerable))
-    )(
-      true
-    )(
-      matcher
-    )
-  }
+import reduceWithValueKey from "@unction/reducewithvaluekey";
+import dig from "@unction/dig";
+import arrayify from "@unction/arrayify";
+export default function where(matcher) {
+  return function whereMatcher(keyedEnumerable) {
+    return reduceWithValueKey(latest => value => key => latest && value(dig(arrayify(key))(keyedEnumerable)))(true)(matcher);
+  };
 }
